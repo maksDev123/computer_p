3c55a9bd7b52af1ee25b4c128b5c961f83afb1f0
maksDev123 <maksym.buleshnyi.pn@ucu.edu.ua>
2022-12-21 10:44:41 +0200

    Initial commit

---
+++ computer_project.py
@@ -0,0 +1,21 @@
+'''
+Computer project
+'''
+
+def task_1():
+    pass
+
+def task_2():
+    pass
+
+def task_3():
+    pass
+
+def task_4():
+    pass
+
+def task_5():
+    pass
+
+def task_6():
+    pass
